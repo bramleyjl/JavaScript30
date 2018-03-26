49dfcb807b7e484ad2cbc374fbb04ac1949bb5b8
John Bramley <bramleyjl@gmail.com>
2018-03-26 14:18:46 -0400

    project 01 - JS Drum Kit completed

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,18 @@
+  function playSound(event) { 
+    var audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    var key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+  }
+
+  function removeTransition(event) {
+    if (event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+  }
+
+  window.addEventListener('keydown', playSound);
+  
+  const keys = document.querySelectorAll(`.key`);
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
